1a707f5d97c680d01416cefb09cb556462d9fb53
Obedyhaw <asanteyawobed@gmail.com>
2023-02-06 11:29:10 +0000

    Multiplication PY Code

---
+++ Multiplication timetable.py
@@ -0,0 +1,10 @@
+welcome=print("Welcome to YhawTech Timetable")
+name=input("What is your name?")
+ourNum = int(input("Which number do you want the Multiplication timetable for?:"))
+ourRange = range(1,13)
+for x in ourRange:
+    result = ourNum * x
+    print(ourNum, " * ",x,"=",result)
+
+
+
